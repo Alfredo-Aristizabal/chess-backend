13da9dfce808deb63267cc571c70ee3bca8997c1
Alfredo-Aristizabal <divinebizero@gmail.com>
2023-07-02 14:49:57 -0400

    Add cors policy

--- Program.cs
+++ Program.cs
@@ -10,7 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddGraphQLServer().AddQueryType<UserData>().AddProjections().AddFiltering().AddSorting();
-
+builder.Services.AddCors(o => o.AddPolicy("MyPolicy", b =>
+{
+    b.WithOrigins("http://localhost:3000")
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+}));
 
 builder.Services.AddDbContext<ChessBackendDbContext>(options => options.UseNpgsql("name=DefaultConnection"));
 var app = builder.Build();
@@ -21,7 +26,7 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseCors("MyPolicy");
 app.MapControllers();
 app.MapGraphQL("/graphql");
 
